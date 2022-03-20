e0e5d99cb09f6320c2c06db757b88c49463b2ede
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2022-03-20 20:42:06 +0100

    init

---
+++ src/plugins/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const md5 = require('md5');
+const apiTS = 1
+
+export const marvelPublic = axios.create({
+  baseURL: `${process.env.REACT_APP_API_MARVEL}`,
+  params: {
+    ts: apiTS ,
+    apikey: `${process.env.REACT_APP_API_KEY_PUBLIC_MARVEL}`,
+    hash: md5(apiTS+`${process.env.REACT_APP_API_KEY_PRIVATE_MARVEL}`+`${process.env.REACT_APP_API_KEY_PUBLIC_MARVEL}`)
+  }
+});
