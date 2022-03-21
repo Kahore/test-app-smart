57c9e1a90e01f682e1f807b1e2339f5ebc19930a
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2022-03-21 10:50:25 +0100

    404

--- src/view/NotFound.tsx
+++ src/view/NotFound.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 const NotFoundView = () => {
-  return <>NotFoundView</>;
+  return (
+    <Container className={"Root__top-container"}>
+      <Box component="main" sx={{ flexGrow: 1 }} className={"Root__main-view"}>
+        <Typography variant={"h5"}>
+          Oops! Nothing, except a bad news for you.
+        </Typography>
+        <Typography variant={"subtitle2"}>
+          The page you are looking for does not exist
+        </Typography>
+        <NavLink to={"/"}>
+          <Button variant={"contained"} color={"primary"}>
+            Back
+          </Button>
+        </NavLink>
+      </Box>
+    </Container>
+  );
 };
 export default NotFoundView;
